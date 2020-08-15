3397673ee9d8e331754c5b334ce7c547bb9acdf9
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-14 19:37:32 -0700

    table: extracted Trow to it's own component

--- src/components/Tbody/index.js
+++ src/components/Tbody/index.js
@@ -1,32 +1,11 @@
 import React from "react";
+import Trow from "../Trow";
 
 function Tbody() {
   return (
     <tbody>
-      <tr>
-        <th>1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th>2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th>3</th>
-        <td>Larry the Bird</td>
-        <td>@twitter</td>
-        <td>@mdo</td>
-        <td>@mdo</td>
-      </tr>
+      <Trow />
     </tbody>
-
-
   );
 }
 
---
+++ src/components/Trow/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Tbody() {
+  return (
+    <tr>
+      <th>1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+  );
+}
+
+export default Tbody;
