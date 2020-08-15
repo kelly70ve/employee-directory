f66f5681cc02cd02788c5aa47bfcf0d035d9439e
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-14 18:04:54 -0700

    navbar: add static nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-light bg-light d-flex justify-content-center bg-dark">
+      <span className="mb-0 text-white display-4 p-2">Employee Directory</span>
+    </nav>
+  );
+}
+
+export default Navbar;
