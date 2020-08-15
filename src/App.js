c0a295135923386c10eea9d963cf5b3400f50400
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-14 20:07:13 -0700

    api: get employee data from api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import Navbar from "./components/Navbar.js";
+import React, { Component }  from 'react';
+import Navbar from "./components/Navbar";
 import Input from "./components/Input";
 import Wrapper from "./components/Wrapper";
 import Table from "./components/Table";
+import API from "./utils/API";
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Wrapper>
-        <Input />
-        <Table />
-      </Wrapper>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    employees: []
+  }
+
+  componentDidMount() {
+    API.search()
+    .then(res => this.setState({ employees: res.data.results }))
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Input />
+          <Table />
+        </Wrapper>
+      </div>
+    );
+  }
 }
 
 export default App;
