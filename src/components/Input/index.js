b09b058e50b9366c618871011190d355d2007f12
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-17 15:45:56 -0700

    Table: add alt text to images

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Input(props) {
   return (
     <div className="input-group mt-4">
-      <input type="text" className="form-control" placeholder="Search Employees" aria-label="Search" aria-describedby="basic-addon1" 
+      <input className="form-control" placeholder="Search Employees" aria-label="Search" aria-describedby="basic-addon1" 
       value={props.value}
       name={props.name}
       onChange={props.onChange}
