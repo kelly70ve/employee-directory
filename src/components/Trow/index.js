c019db3fc5cfa076f4504c77f3ecd4738bb223e7
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-17 15:46:29 -0700

    Table: type was doubled

--- src/components/Trow/index.js
+++ src/components/Trow/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Trow(props) {
   return (
     <tr>
-      <td><img src={props.image} /></td>
+      <td><img src={props.image} alt={props.name}/></td>
       <td>{props.name}</td>
       <td>{props.phone}</td>
       <td><a href={'mailto:' + props.email}>{props.email}</a></td>
